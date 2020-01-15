6ff0882500bd4fa6b58f39643134355a1afec68b
peterckim <peterckim96@gmail.com>
2020-01-15 00:13:41 -0500

    added dotenv

--- api/sequelize.js
+++ api/sequelize.js
@@ -2,29 +2,19 @@ const Sequelize = require("sequelize");
 const keys = require("../config/config");
 
 /* Connect to MS SQL Server db */
-const sequelize = new Sequelize(keys.db, keys.user, keys.password, {
-  host: keys.host,
-  dialect: "mssql",
-  dialectOptions: {
-    options: {
-      encrypt: true
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: "mssql",
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
     }
   }
-});
-
-// const sequelize = new Sequelize(
-//   process.env.DATABASE,
-//   process.env.USER,
-//   process.env.PASSWORD,
-//   {
-//     host: process.env.HOST,
-//     dialect: "mssql",
-//     dialectOptions: {
-//       options: {
-//         encrypt: true
-//       }
-//     }
-//   }
-// );
+);
 
 module.exports = sequelize;
